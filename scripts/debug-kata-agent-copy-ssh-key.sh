5075a08b82ed305842552b28bed9e5046cd00c58
Ilya Dmitrichenko <errordeveloper@gmail.com>
2020-04-21 19:57:00 +0100

    Move misc script out of root dir

--- scripts/debug-kata-agent-copy-ssh-key.sh
+++ scripts/debug-kata-agent-copy-ssh-key.sh
@@ -4,6 +4,10 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
+# kubectl cp is slow on kata, so we have this
+# it depends on kata agent image being built with
+# an SSH key (see `images/kata-agent-ubuntu/Makefile`)
+
 kubectl exec -ti deployment/test-cluster-master bash -- -c "
 cat > id_rsa << EOF
 $(cat ~/.ssh/id_rsa)
