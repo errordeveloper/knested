82cc67691d054ad49d40718d6b81c85ce0f9f599
Ilya Dmitrichenko <idmitrichenko@coreweave.com>
2025-05-22 10:46:00 +0100

    Include kind and name in resource keys

--- scripts/import-chart.sh
+++ scripts/import-chart.sh
@@ -9,7 +9,7 @@ dir="$(mktemp -d "${PWD}/import-chart.XXXXXX")"
 helm template --output-dir="${dir}" "$@" >/dev/null
 
 resources=($(find "${dir}" -type f -name '*.yaml'))
-path_expr="$(printf 'strings.TrimSuffix(strings.Join(strings.Split(path.Rel("%s", filename), "/"), "-"), ".yaml")' "${dir}")"
+path_expr="$(printf 'strings.TrimSuffix(strings.Join(strings.Split(path.Rel("%s", filename), "/"), "-"), ".yaml") + "-" + strings.ToLower(data.kind) + "-" + strings.ToLower(data.metadata.name)' "${dir}")"
 
 cue import --outfile - --with-context --path "${path_expr}" "${resources[@]}"
 
