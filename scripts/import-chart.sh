695799b835072da77e89ce12bb654c2b8d1f5bf0
Ilya Dmitrichenko <idmitrichenko@coreweave.com>
2025-05-22 10:51:47 +0100

    Hash the data instead of assuming the schema

--- scripts/import-chart.sh
+++ scripts/import-chart.sh
@@ -9,7 +9,7 @@ dir="$(mktemp -d "${PWD}/import-chart.XXXXXX")"
 helm template --output-dir="${dir}" "$@" >/dev/null
 
 resources=($(find "${dir}" -type f -name '*.yaml'))
-path_expr="$(printf 'strings.TrimSuffix(strings.Join(strings.Split(path.Rel("%s", filename), "/"), "-"), ".yaml") + "-" + strings.ToLower(data.kind) + "-" + strings.ToLower(data.metadata.name)' "${dir}")"
+path_expr="$(printf 'strings.TrimSuffix(strings.Join(strings.Split(path.Rel("%s", filename), "/"), "-"), ".yaml") + "-" + hex.Encode(sha256.Sum256(json.Marshal(data)))' "${dir}")"
 
 cue import --outfile - --with-context --path "${path_expr}" "${resources[@]}"
 
